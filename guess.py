e18ef617a80885474e723453eed31926b3843d53
charlyecastro <castroc@uw.edu>
2020-02-05 14:04:07 -0800

    added eves guessing capability, added user guessing flexibility and added play again funcitonality

--- guess.py
+++ guess.py
@@ -10,6 +10,7 @@ from time import ctime
 rec = sr.Recognizer()
 random_number = 100
 MAX_NUMBER = 100
+MIN_NUMBER = 1
 eves_guess = 0
 
 # Using the microphone, Eve listens to the user and returns a String Representations of what the user said. If eve can understand it will return 'Sorry, I did not get that'. If the speech service fails it will say 'Sorry, my speech service is down'
@@ -36,20 +37,16 @@ def speak(audio_string):
     os.remove(audio_file)
 
 # Eve introduces herslef and invites the user to play the guessing game. If the user answers yes, eve will then ask if the user wants to be the guesser or not, Otherwise Eve will say goodbye. 
-def game_intro():
-    time.sleep(1)
-    speak("Hi my name is Eve. Do you want to play?")
+def game_intro(message = "Hi my name is Eve. Do you want to play the number guessing game?"):
+    speak(message)
     game_response = listen()
     if 'yes' in game_response:
-        speak("do you want to guess?")
+        speak("Do you want to be the guesser?")
         guess_response = listen()
         if 'yes' in guess_response:
             user_guesses()
         else:
-            speak("Okay, think of a number between 1 and 100.")
-            time.sleep(3)
-            speak("Ready?")
-            #eve_guesses()
+            eve_guesses()
     else:
         speak("Okay, maybe next time.")
         exit()
@@ -60,13 +57,38 @@ def user_guesses():
     random_number = random.randint(1, MAX_NUMBER)
     speak("Okay, I am thinking of a number between 1 and 100. Guess what it is.")
     while users_guess != random_number:
-        users_guess = listen()
-        users_guess = int(users_guess)
+        users_response = listen()
+        users_guess = 0
+        for s in users_response.split():
+            if s.isdigit():
+                users_guess = int(s)
+                break
         if users_guess > random_number:
-            speak("The number I am thinking is less than " + users_guess)
-        if users_guess < random_number:
-            speak("The number I am thinking is greater than " + users_guess)
+            speak("The number I am thinking is less than " + str(users_guess))
+        elif users_guess < random_number:
+            speak("The number I am thinking is greater than " + str(users_guess))
     speak("Great Job! you guessed it!")
+    game_intro("Do you want to play again?")
 
 # The user picks a random number and listens to Eve's guesses until the Eve guesses the right number. Eve also keeps track of the number of guesses. Depending on the Eve's guess, the user will let Eve know if their guess is too high or too low.
-#def eve_guesses()
+def eve_guesses():
+    max_num = 100
+    min_num = 1
+    found = False
+    speak("Okay, think of a number between 1 and 100! If my guess is wrong please tell me if the number is greater than or less than my guess")
+    while not found:
+        eve_guess = random.randint(min_num, max_num)
+        speak("Is it " + str(eve_guess))
+        users_response = listen()
+        if 'less than' in users_response:
+            max_num = eve_guess - 1
+        elif 'greater than' in users_response:
+            min_num = eve_guess + 1
+        elif 'yes' in users_response:
+            break
+    speak("Hooray! I got it")
+    game_intro("Do you want to play again?")
+
+
+# Play The game
+game_intro()
